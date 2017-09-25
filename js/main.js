a45b33b0e3f01d075310c815c28ff4ba52f82a55
mbmosman <mbMosman@users.noreply.github.com>
2017-09-25 05:57:21 -0500

    updates to fix load

--- js/main.js
+++ js/main.js
@@ -2,50 +2,62 @@ import WebFlashcardApp from "js/webflashcards/web_flashcards_app";
 $(function () {
   
   const wfApp = new WebFlashcardApp("cardapp");
+  let appStarted = false;
   wfApp.init();
-  if (wfApp.getSessionCardCount() > 0){
-    $("#welcome").hide();
+  if (wfApp.getSessionCardCount() < 1){
+    $("#welcome").show();
   }
   
   $("#app-start").on("click", function() {
     if (wfApp.getSessionCardCount() > 0){
+      $("#app-start").hide();
+      $("#welcome").hide();
       wfApp.startSession();
-      $("#welcome").show();
     }
   });
   
   $("#joyful-jp-l1").on("click", function() {
     $.getJSON("data/joyful-jp-l1.json", function(data) {
       wfApp.addNewCards(data); 
-      $("#app-start").show();
+      if (!appStarted) {
+        $("#app-start").show();
+      }
     });
   });
   
   $("#joyful-jp-l2").on("click", function() {
     $.getJSON("data/joyful-jp-l2.json", function(data) {
       wfApp.addNewCards(data); 
-      $("#app-start").show(); 
+      if (!appStarted) {
+        $("#app-start").show();
+      }
     });
   });
 
   $("#joyful-jp-l3").on("click", function() {
     $.getJSON("data/joyful-jp-l3.json", function(data) {
-      wfApp.addNewCards(data); 
-      $("#app-start").show(); 
+      wfApp.addNewCards(data);  
+      if (!appStarted) {
+        $("#app-start").show();
+      }
     });
   });  
   
   $("#joyful-jp-travel-l1").on("click", function() {
     $.getJSON("data/joyful-jp-travel-l1.json", function(data) {
-      wfApp.addNewCards(data);  
-      $("#app-start").toggle();
+      wfApp.addNewCards(data); 
+      if (!appStarted) {
+        $("#app-start").show();
+      }
     });
   });
 
   $("#joyful-jp-travel-l3").on("click", function() {
     $.getJSON("data/joyful-jp-travel-l3.json", function(data) {
       wfApp.addNewCards(data); 
-      $("#app-start").toggle(); 
+      if (!appStarted) {
+        $("#app-start").show();
+      } 
     });
   });
 });
